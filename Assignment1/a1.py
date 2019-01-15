6f2a8e96a208afc9fcdb518b29924191cc6ae570
Jichun Li <jichunli@uw.edu>
2019-01-14 23:18:48 -0800

    a

--- Assignment1/a1.py
+++ Assignment1/a1.py
@@ -8,13 +8,14 @@ def five_x_cubed_plus_1(x):
 	return 5 * (x ** 3) + 1
 
 #2
-def pair_off(n):
-    pairs = []
-    for x in range(int (len(n) / 2)):
-        pairs.append([n[x * 2], n[x * 2 + 1]])
-    if (int (len(n) % 2) == 1):
-        pairs.append([n[-1]])
-    return pairs
+def pair_off(ary):
+    result = []
+    
+    for i in range(0, int(len(ary) / 2 * 2), 2):
+        result.append([ary[i], ary[i + 1]])
+    if (int (len(ary) % 2) == 1):
+        result.append([ary[-1]])
+    return result
 
 #3
 def mystery_code(input_string):
